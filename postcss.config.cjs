372039c2a2b980a1f98593c348f81e92e81e1b82
Christina Maher <christina.maher@example.com>
2025-05-10 11:39:00 -0700

    chore: Update PostCSS and Tailwind CSS dependencies; refactor PostCSS config and remove unused Tailwind config file

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,8 @@
-const tailwindcss = require('tailwindcss')
-const postcssNesting = require('postcss-nesting')
-const autoprefixer = require('autoprefixer')
-
 /** @type {import('postcss').Config} */
 module.exports = {
-  plugins: [
-    tailwindcss,
-    postcssNesting(),
-    autoprefixer
-  ]
+  plugins: {
+    'tailwindcss': {},
+    'postcss-nesting': {},
+    'autoprefixer': {}
+  }
 }
